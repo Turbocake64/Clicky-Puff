0b70b08fe46515e7f8867ec4429a958c5fac733b
Turbocake64 <micahrabinowitz@gmail.com>
2019-04-28 12:56:39 -0700

    character chards built

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CharacterCard from "./components/CharacterCard";
+// import Navbar from "./component/Navbar";
+import Title from "./components/Title";
+import Wrapper from "./components/Wrapper";
+import characters from "./characters.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Clicky Puff! Do-da-do do-doo!.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.characters to the character json array
+  state = {
+    characters
+  };
+
+  // Map over this.state.characters and render a CharacterCard component for each character object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Clicky Puff!</Title>
+        {this.state.characters.map(character => (
+          <CharacterCard
+            removeFriend={this.removeFriend}
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
